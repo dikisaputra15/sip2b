11feb5ff52e20d536148513021cd40f5a799c089
dikisaputra15 <mbpretina@192.168.1.34>
2023-10-01 13:41:11 +0700

    selesai update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,38 +9,18 @@
         <h3>Dashboard Monitoring Pengawasan Barang</h3>
     </div>
     <div class="card-body">
-<<<<<<< HEAD
-
-    <div class="row">
-=======
          <div class="row">
->>>>>>> a34af503aa00ac83630779fdf6dcee5126d6b7aa
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-<<<<<<< HEAD
-                <p>Barang Masuk</p>
-=======
 
                 <p>Stok Barang</p>
->>>>>>> a34af503aa00ac83630779fdf6dcee5126d6b7aa
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
               </div>
-<<<<<<< HEAD
-              <a href="/admin/barangmasuk" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <p>Barang Keluar</p>
-=======
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="/admin/stokbarang" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
 
@@ -50,49 +30,27 @@
               <div class="inner">
 
                 <p>Barang Masuk</p>
->>>>>>> a34af503aa00ac83630779fdf6dcee5126d6b7aa
               </div>
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
               </div>
-<<<<<<< HEAD
-              <a href="/admin/barangkeluar" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="/admin/barangmasuk" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
-        
+          <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-danger">
               <div class="inner">
-                <p>Stok Barang</p>
+                <p>Barang Keluar</p>
               </div>
               <div class="icon">
                 <i class="ion ion-pie-graph"></i>
               </div>
-              <a href="/admin/stokbarang" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="/admin/barangkeluar" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
-          <!-- ./col -->
-        </div>
-=======
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
 
-                <p>Barang Keluar</p>
-              </div>
-              <div class="icon">
-                 <i class="ion ion-stats-bars"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
->>>>>>> a34af503aa00ac83630779fdf6dcee5126d6b7aa
 
     </div>
 </div>
